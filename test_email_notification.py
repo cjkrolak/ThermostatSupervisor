f2b8687e013058b0f86e2a49913a4b33b97d03a4
Christopher Krolak <cjkrolak@gmail.com>
2021-02-06 18:09:29 -0600

    use runtime parameters instead of env variables for unit test

--- test_email_notification.py
+++ test_email_notification.py
@@ -3,6 +3,7 @@ Unit tes module for email_notification.py.
 """
 
 import email_notification as eml
+import sys
 import unittest
 
 
@@ -15,9 +16,20 @@ class Test(unittest.TestCase):
         pass
 
     def testName(self):
+        if len(sys.argv) >= 3:
+            to_address = sys.argv[0]
+            from_address = sys.argv[1]
+            from_password = sys.argv[2]
+        else:
+            to_address = None
+            from_address = None
+            from_password = None
         body = "this is a test of the email notification alert."
         return_status = \
-            eml.send_email_alert(subject="(unittest) test email alert",
+            eml.send_email_alert(to_address=to_address,
+                                 from_address=from_address,
+                                 from_password=from_password,
+                                 subject="(unittest) test email alert",
                                  body=body,
                                  debug=True)
         self.assertEqual(return_status, eml.NO_ERROR)
@@ -25,4 +37,6 @@ class Test(unittest.TestCase):
 
 if __name__ == "__main__":
     # import sys;sys.argv = ['', 'Test.testName']
+
+        
     unittest.main()
