b2c24d8d745120f7fe51f56bbf124645f8013a84
Christopher Krolak <cjkrolak@gmail.com>
2021-03-24 16:47:51 -0500

    #8 flask server now working through IDE, not yet working through cmd line

--- flaskserver.py
+++ flaskserver.py
@@ -1,46 +1,45 @@
 """
-Flask server for posting supervisor status messages.
+Flask server for displaying supervisor output on web page.
 """
-
-
-from contextlib import redirect_stdout
-import flask
-import subprocess
-import time
+import html
+from subprocess import Popen, PIPE, STDOUT, DEVNULL
+import sys
 import webbrowser
 
-# script to run
-path = '/honeywell.py'
-console_log_file = "honeywell_status.txt"
+from flask import Flask, Response
+app = Flask(__name__)
 
-app = flask.Flask(__name__)
+# port for viewing flask
+flask_port = 80
 
 
-@app.route('/yield')
+@app.route('/')
 def index():
-    def inner():
-        proc = subprocess.Popen(
-            (path,),
-            shell=True,
-            stdout=subprocess.PIPE
-        )
+    def g():
+        yield "<!doctype html><title>Stream subprocess output</title>"
 
-        for line in iter(proc.stdout.readline, ''):
-            print("console: " + line)
-            time.sleep(10)  # Don't need this just shows the text streaming
-            yield line.rstrip() + '<br/>\n'
+        # runtime variables
+        executable = "python"
+        script = "supervise.py"
+        if len(sys.argv) > 1:
+            arg_list = [executable, script] + sys.argv[1:]
+        else:
+            arg_list = [executable, script]
+        print("just before popen command")
+        with Popen(arg_list, stdin=DEVNULL, stdout=PIPE, stderr=STDOUT,
+                   bufsize=1, universal_newlines=True, shell=True) as p:
+            print("in popen command: %s" % type(p.stdout))
+            i = 0
+            for line in p.stdout:
+                print("line %s" % i)
+                i += 1
+                yield "<code>{}</code>".format(html.escape(line.rstrip("\n")))
+                yield "<br>\n"
+    print("returning g")
+    return Response(g(), mimetype='text/html')
 
-    return flask.Response(inner(), mimetype='text/html')
-    # text/html is required for most browsers to show th$
 
-
-if __name__ == "__main__":
-
-    # redirect console output to file for posting
-    with open(console_log_file, 'w') as f:
-        with redirect_stdout(f):
-            print("it now prints to %s" % console_log_file)
-
+if __name__ == '__main__':
     # show the page in browser
-    webbrowser.open('http://localhost:23423')
-    app.run(host='localhost', port=23423, debug=True)
+    webbrowser.open('http://127.0.0.1:' + str(flask_port))
+    app.run(host='127.0.0.1', port=flask_port)
