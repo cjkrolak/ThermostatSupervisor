dd3702dd082449daa521bf24adbdbb3266fdffc7
Christopher Krolak <cjkrolak@gmail.com>
2021-07-29 21:31:03 -0500

    enhanced test name printing for readability

--- unit_test_common.py
+++ unit_test_common.py
@@ -8,4 +8,7 @@ import utilities as util
 
 def print_test_name():
     """Print out the unit test name to the console."""
+    print("\n")
+    print("=" * 40)
     print("testing '%s'" % util.get_function_name(2))
+    print("=" * 40)
