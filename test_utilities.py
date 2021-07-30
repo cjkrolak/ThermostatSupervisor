42d9679de1756fd6835f4cccb3a461b15530f060
Christopher Krolak <cjkrolak@gmail.com>
2021-07-29 23:10:03 -0500

    resolves #87 updated test_utilities to full coverage

--- test_utilities.py
+++ test_utilities.py
@@ -19,6 +19,49 @@ class Test(unittest.TestCase):
     def tearDown(self):
         pass
 
+    def testGetEnvVariable(self):
+        """
+        Confirm get_env_variable() can retrieve values.
+        """
+        utc.print_test_name()
+        for env_key in ["GMAIL_USERNAME", "GMAIL_PASSWORD",
+                        "GMAIL_TO_USERNAME", "TCC_USERNAME",
+                        "TCC_PASSWORD"]:
+            buff = util.get_env_variable(env_key)
+            print("env$%s=%s" % (env_key,
+                                 [buff["value"],
+                                  "(hidden)"]['PASSWORD' in env_key]))
+            self.assertEqual(buff["status"], util.NO_ERROR)
+            self.assertGreater(len(buff["value"]), 0)
+
+    def testGetFunctionName(self):
+        """
+        Confirm get_function_name works as expected.
+        """
+        # default
+        test = "<default>"
+        print("testing util.get_function_name(%s)" % test)
+        ev_1 = "testGetFunctionName"
+        result_1 = util.get_function_name()
+        self.assertEqual(ev_1, result_1, "expected=%s, actual=%s" %
+                         (ev_1, result_1))
+
+        # test 1
+        test = 1
+        print("testing util.get_function_name(%s)" % test)
+        ev_1 = "testGetFunctionName"
+        result_1 = util.get_function_name(test)
+        self.assertEqual(ev_1, result_1, "test%s: expected=%s, actual=%s" %
+                         (test, ev_1, result_1))
+
+        # test 2
+        test = 2
+        print("testing util.get_function_name(%s)" % test)
+        ev_1 = "_callTestMethod"
+        result_1 = util.get_function_name(test)
+        self.assertEqual(ev_1, result_1, "test%s: expected=%s, actual=%s" %
+                         (test, ev_1, result_1))
+
     def testLogMsgWrite(self):
         """
         Confirm log_msg() can write and append to file.
@@ -60,18 +103,28 @@ class Test(unittest.TestCase):
         #                 test_msg1_length + test_msg2_length)
         self.assertGreater(file_size_bytes, 60)
 
-    def testGetEnvVariable(self):
+    def testGetFullFilePath(self):
         """
-        Confirm get_env_variable() can retrieve values.
+        Verify get_full_file_path() function.
         """
-        utc.print_test_name()
-        for env_key in ["GMAIL_USERNAME", "GMAIL_PASSWORD",
-                        "GMAIL_TO_USERNAME", "TCC_USERNAME",
-                        "TCC_PASSWORD"]:
-            buff = util.get_env_variable(env_key)
-            print("env$%s=%s" % (env_key, buff["value"]))
-            self.assertEqual(buff["status"], util.NO_ERROR)
-            self.assertGreater(len(buff["value"]), 0)
+        file_name = "dummy.txt"
+        full_path = util.get_full_file_path(file_name)
+        expected_value = util.file_path + "//" + file_name
+        self.assertEqual(expected_value, full_path,
+                         "expected=%s, actual=%s" %
+                         (expected_value, full_path))
+
+    def testutf8len(self):
+        """
+        Verify utf8len().
+        """
+        for test_case in ["A", "BB", "ccc", "dd_d"]:
+            print("testing util.utf8len(%s)" % test_case)
+            expected_value = 1 * len(test_case)
+            actual_value = util.utf8len(test_case)
+            self.assertEqual(expected_value, actual_value,
+                             "expected=%s, actual=%s" %
+                             (expected_value, actual_value))
 
 
 if __name__ == "__main__":
