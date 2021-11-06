7f9ff6d65e738910b052455299eb8a4df0ae4310
Christopher Krolak <cjkrolak@gmail.com>
2021-11-06 07:35:22 -0500

    resolves #167, finished test_get_current_mode

--- test_thermostat_common.py
+++ test_thermostat_common.py
@@ -120,6 +120,7 @@ class Test(unittest.TestCase):
 
     def test_ValidateNumeric(self):
         """Test validate_numeric() function."""
+        utc.print_test_name()
         for test_case in [1, 1.0, "1", True]:
             print("test case=%s" % type(test_case))
             if isinstance(test_case, (int, float)):
@@ -138,6 +139,7 @@ class Test(unittest.TestCase):
 
     def test_WarnIfOutsideGlobalLimit(self):
         """Test warn_if_outside_global_limit() function."""
+        utc.print_test_name()
         self.assertTrue(self.Zone.warn_if_outside_global_limit(
             2, 1, operator.gt, "heat"),
             "function result should have been True")
@@ -162,43 +164,185 @@ class Test(unittest.TestCase):
         4. cool mode and cool deviation
         5. humidity is available
         """
+        test_cases = {
+            "heat mode and following schedule": {
+                "mode": "heat_mode",
+                "humidity": False,
+                "heat_mode": True,
+                "cool_mode": False,
+                "heat_deviation": False,
+                "cool_deviation": False,
+                "hold_mode": False,
+                },
+            "heat mode and following schedule and humidity": {
+                "mode": "heat_mode",
+                "humidity": True,
+                "heat_mode": True,
+                "cool_mode": False,
+                "heat_deviation": False,
+                "cool_deviation": False,
+                "hold_mode": False,
+                },
+            "heat mode and deviation": {
+                "mode": "heat_mode",
+                "humidity": False,
+                "heat_mode": True,
+                "cool_mode": False,
+                "heat_deviation": True,
+                "cool_deviation": False,
+                "hold_mode": True,
+                },
+            "heat mode and deviation and humidity": {
+                "mode": "heat_mode",
+                "humidity": True,
+                "heat_mode": True,
+                "cool_mode": False,
+                "heat_deviation": True,
+                "cool_deviation": False,
+                "hold_mode": True,
+                },
+            "cool mode and following schedule": {
+                "mode": "cool_mode",
+                "humidity": False,
+                "heat_mode": False,
+                "cool_mode": True,
+                "heat_deviation": False,
+                "cool_deviation": False,
+                "hold_mode": False,
+                },
+            "cool mode and following schedule and humidity": {
+                "mode": "cool_mode",
+                "humidity": True,
+                "heat_mode": False,
+                "cool_mode": True,
+                "heat_deviation": False,
+                "cool_deviation": False,
+                "hold_mode": False,
+                },
+            "cool mode and deviation": {
+                "mode": "cool_mode",
+                "humidity": False,
+                "heat_mode": False,
+                "cool_mode": True,
+                "heat_deviation": False,
+                "cool_deviation": True,
+                "hold_mode": True,
+                },
+            "cool mode and deviation and humidity": {
+                "mode": "cool_mode",
+                "humidity": True,
+                "heat_mode": False,
+                "cool_mode": True,
+                "heat_deviation": False,
+                "cool_deviation": True,
+                "hold_mode": True,
+                },
+            }
         utc.print_test_name()
-        return  # test is not ready
+        # return  # test is not ready
         self.backup_functions()
         try:
-            # heat mode and following schedule
-            self.Zone.get_system_switch_position = \
-                (lambda *_, **__:
-                 self.Zone.system_switch_position[
-                     self.Zone.HEAT_MODE])
-            ret_dict = self.Zone.get_current_mode(1, 1, True, False)
+            for test_case in test_cases:
+                # mock up mode, set points, and humidity setting
+                self.mock_set_mode(test_cases[test_case]["mode"])
+                self.mock_set_points(test_cases[test_case]["heat_deviation"],
+                                     test_cases[test_case]["cool_deviation"])
+                self.mock_set_humidity_support(
+                    test_cases[test_case]["humidity"])
 
-            # heat mode and deviation
-            self.Zone.get_system_switch_position = \
-                (lambda *_, **__:
-                 self.Zone.system_switch_position[
-                     self.Zone.HEAT_MODE])
-            ret_dict = self.Zone.get_current_mode(1, 1, True, False)
-            # cool mode and following schedule
-            # self.Zone.get_system_switch_position() = \
-            #    self.Zone.system_switch_position[self.COOL_MODE]
-            ret_dict = self.Zone.get_current_mode(1, 1, True, False)
-            print("%s" % ret_dict)
-            # cool mode and cool deviation
+                # call function and print return value
+                ret_dict = self.Zone.get_current_mode(1, 1, True, False)
+                print("test case '%s' result: '%s'" % (test_case, ret_dict))
+
+                # verify return states are correct
+                for return_val in ["heat_mode", "cool_mode", "heat_deviation",
+                                   "cool_deviation", "hold_mode"]:
+                    self.assertEqual(ret_dict[return_val],
+                                     test_cases[test_case][return_val],
+                                     "test case '%s' parameter '%s', "
+                                     "result=%s, expected=%s" %
+                                     (test_case, return_val,
+                                      ret_dict[return_val],
+                                      test_cases[test_case][return_val]))
+
+                # verify humidity reporting
+                if test_cases[test_case]["humidity"]:
+                    self.assertTrue("humidity" in ret_dict["status_msg"])
+                else:
+                    self.assertTrue("humidity" not in ret_dict["status_msg"])
         finally:
             self.restore_functions()
+
+    def mock_set_mode(self, mock_mode):
+        """
+        Mock heat setting by overriding switch position function.
+
+        Make sure to backup and restore methods if using this function.
+
+        inputs:
+            mock_mode(str): mode string
+        returns:
+            None
+        """
+        self.Zone.is_heat_mode = \
+            (lambda *_, **__: mock_mode == "heat_mode")
+        self.Zone.is_cool_mode = \
+            (lambda *_, **__: mock_mode == "cool_mode")
+
+    def mock_set_points(self, heat_deviation, cool_deviation):
+        """
+        Override heat and cool set points with mock values.
+
+        inputs:
+            heat_deviation(bool): True if heat is deviated
+            cool_deviation(bool): True if cool is deviated
+        returns:
+            None
+        """
+        deviation_val = self.Zone.tolerance_degrees + 1
+        heat_sched_sp = self.Zone.max_scheduled_heat_allowed - 13
+        heat_sp = heat_sched_sp + [0, deviation_val][heat_deviation]
+        cool_sched_sp = self.Zone.min_scheduled_cool_allowed + 13
+        cool_sp = cool_sched_sp - [0, deviation_val][cool_deviation]
+
+        self.Zone.get_heat_setpoint_raw = (lambda *_, **__: heat_sp)
+        self.Zone.get_schedule_heat_sp = (lambda *_, **__: heat_sched_sp)
+        self.Zone.get_cool_setpoint_raw = (lambda *_, **__: cool_sp)
+        self.Zone.get_schedule_cool_sp = (lambda *_, **__: cool_sched_sp)
+
+    def mock_set_humidity_support(self, bool_val):
+        """
+        Mock humidity support.
+
+        inputs:
+            bool_val(bool): humidity support state
+        returns:
+            None
+        """
+        self.Zone.get_is_humidity_supported = \
+            (lambda *_, **__: bool_val)
 
     def backup_functions(self):
         """Backup functions prior to mocking return values."""
         self.switch_pos_bckup = self.Zone.get_system_switch_position
+        self.is_heat_mode_bckup = self.Zone.is_heat_mode
+        self.is_cool_mode_bckup = self.Zone.is_cool_mode
         self.heat_raw_bckup = self.Zone.get_heat_setpoint_raw
         self.schedule_heat_sp_bckup = self.Zone.get_schedule_heat_sp
+        self.cool_raw_bckup = self.Zone.get_cool_setpoint_raw
+        self.schedule_cool_sp_bckup = self.Zone.get_schedule_cool_sp
+        self.get_humid_support_bckup = self.Zone.get_is_humidity_supported
 
     def restore_functions(self):
         """Restore backed up functions."""
         self.Zone.get_system_switch_position = self.switch_pos_bckup
+        self.Zone.is_heat_mode = self.is_heat_mode_bckup
+        self.Zone.is_cool_mode = self.is_cool_mode_bckup
         self.Zone.get_heat_setpoint_raw = self.heat_raw_bckup
         self.Zone.get_schedule_heat_sp = self.schedule_heat_sp_bckup
+        self.Zone.get_cool_setpoint_raw = self.cool_raw_bckup
+        self.Zone.get_schedule_cool_sp = self.schedule_cool_sp_bckup
+        self.Zone.get_is_humidity_supported = self.get_humid_support_bckup
 
 
 if __name__ == "__main__":
