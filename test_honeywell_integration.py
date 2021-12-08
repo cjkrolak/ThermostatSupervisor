ead7ebce1d9302ee00cdbc2e3363b66b5f706b2a
Christopher Krolak <cjkrolak@gmail.com>
2021-12-07 22:08:53 -0600

    resolves #276 adding Honeywell integration test

---
+++ test_honeywell_integration.py
@@ -0,0 +1,62 @@
+"""
+Integration test module for honeywell.py.
+
+This test requires connection to Honeywell thermostat.
+"""
+# built-in imports
+import unittest
+
+# local imports
+import honeywell
+import honeywell_config
+import supervise as sup
+import thermostat_api as api
+import thermostat_common as tc
+import unit_test_common as utc
+import utilities as util
+
+
+class Test(utc.UnitTestCommon):
+    """Test functions in sht31_flask_server.py."""
+
+    def setUp(self):
+        self.print_test_name()
+
+    def tearDown(self):
+        self.print_test_result()
+
+    def test_HoneywellThermostatBasicCheckout(self):
+        """
+        Verify Honeywell basic checkout function.
+        """
+        tc.thermostat_basic_checkout(api, honeywell_config.ALIAS,
+                                     honeywell.ThermostatClass,
+                                     honeywell.ThermostatZone)
+
+    def test_Z_HoneywellSupervise(self):
+        """
+        Verify supervisor loop on Honeywell Thermostat.
+
+        PYHTCC requests.session() is left open after the test is complete,
+        so this test is titled to run last in the module.
+        """
+        # argv list must be valid settings
+        unit_test_argv = [
+            "supervise.py",  # module
+            "honeywell",  # thermostat
+            "0",  # zone
+            "180",  # poll time in sec
+            "1000",  # reconnect time in sec
+            "2",  # tolerance
+            "",  # thermostat mode, no target
+            "3",  # number of measurements
+            ]
+        return_status = sup.exec_supervise(debug=True,
+                                           argv_list=unit_test_argv)
+        self.assertTrue(return_status, "return status=%s, expected True" %
+                        return_status)
+
+
+if __name__ == "__main__":
+    util.log_msg.debug = True
+    unittest.main(verbosity=2)
