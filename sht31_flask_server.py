92de02340eb60f8134a12b8df6a0531b9482e65b
Christopher Krolak <cjkrolak@gmail.com>
2021-12-11 17:25:34 -0600

    add delays for enable heater and reset functions

--- sht31_flask_server.py
+++ sht31_flask_server.py
@@ -332,7 +332,12 @@ class Sensors(object):
                               i2c_command)
 
             # small delay
-            time.sleep(1.0)
+            if i2c_command in [reset]:
+                time.sleep(5)
+            elif i2c_command in [enable_heater]:
+                time.sleep(3)
+            else:
+                time.sleep(1.0)
 
             # read the measurement data, 2 bytes data, 1 byte checksum
             data = self.read_i2c_data(bus, sht31_config.i2c_address,
