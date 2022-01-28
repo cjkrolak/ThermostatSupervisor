7efd9d9d2cf3ba1e20617faa7037356c3251fb89
Christopher Krolak <cjkrolak@gmail.com>
2022-01-28 08:54:22 -0600

    resolves #435, fixing emulator int tests

--- thermostatsupervisor/emulator_config.py
+++ thermostatsupervisor/emulator_config.py
@@ -2,12 +2,12 @@
 emulator thermostat config file.
 """
 ALIAS = "emulator"
-MAX_HEAT_SETPOINT = 66
-MIN_COOL_SETPOINT = 78
+MAX_HEAT_SETPOINT = 66.0  # float
+MIN_COOL_SETPOINT = 78.0  # float
 STARTING_MODE = "OFF_MODE"  # thermostat set mode when emulator starts
-STARTING_TEMP = 72  # starting temperature when emulator starts
+STARTING_TEMP = 72.0  # starting temperature when emulator starts
 NORMAL_TEMP_VARIATION = 16.0  # reported value variation +/- this value
-STARTING_HUMIDITY = 45  # starting humidity when emulator starts
+STARTING_HUMIDITY = 45.0  # starting humidity when emulator starts
 NORMAL_HUMIDITY_VARIATION = 3.0  # reported val variation +/- this val
 
 # all environment variables required by code should be registered here
