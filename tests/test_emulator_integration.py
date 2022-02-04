daa24e94a2211db4072672df232153ad13410e12
Christopher Krolak <cjkrolak@gmail.com>
2022-02-04 16:54:35 -0600

    resolves #442, adding 0.1 buffer to eliminate false failures

--- tests/test_emulator_integration.py
+++ tests/test_emulator_integration.py
@@ -73,11 +73,12 @@ class PerformanceIntegrationTest(IntegrationTest,
 
         # temperature and humidity repeatability measurements
         # data is uniform distribution, 1 std= (range)/sqrt(12)
+        # adding 0.1 buffer to prevent false failures due to rounding, etc.
         self.temp_stdev_limit = \
-            emulator_config.NORMAL_TEMP_VARIATION * 2 / math.sqrt(12)
+            emulator_config.NORMAL_TEMP_VARIATION * 2 / math.sqrt(12) + 0.1
         self.temp_repeatability_measurements = 100  # number of temp msmts.
         self.humidity_stdev_limit = \
-            emulator_config.NORMAL_HUMIDITY_VARIATION * 2 / math.sqrt(12)
+            emulator_config.NORMAL_HUMIDITY_VARIATION * 2 / math.sqrt(12) + 0.1
         self.humidity_repeatability_measurements = 100  # number of temp msmts.
         self.poll_interval_sec = 0.5  # delay between repeatability msmts
 
