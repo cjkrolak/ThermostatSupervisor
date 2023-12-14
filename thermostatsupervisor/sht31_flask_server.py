bd4a1c36a767ac9c5e4f51f9b77b94dcba3b4796
Christopher Krolak <cjkrolak@gmail.com>
2023-12-14 07:51:49 -0600

    clean-up debug

--- thermostatsupervisor/sht31_flask_server.py
+++ thermostatsupervisor/sht31_flask_server.py
@@ -830,7 +830,6 @@ if __name__ == "__main__":
 
     # parse runtime parameters
     uip = UserInputs()
-    print(f"raw user inputs={uip.user_inputs}")
     debug = uip.get_user_inputs(uip.default_parent_key, "debug")
     print(f"Flask debug mode={debug}", file=sys.stderr)
 
