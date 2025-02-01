57518157dedb3505dd41d2e50b4258d026f8e111
Christopher Krolak <cjkrolak@gmail.com>
2025-01-31 19:02:28 -0600

    removed defaults

--- thermostatsupervisor/sht31_flask_server.py
+++ thermostatsupervisor/sht31_flask_server.py
@@ -794,7 +794,7 @@ def create_app():
     limiter = Limiter(
         get_remote_address,
         app=app_,
-        default_limits=["2 per day", "1 per hour"]
+        default_limits=None
     )
 
     # add API functions
